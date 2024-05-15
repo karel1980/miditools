1f0156922d474fa80c77c31cdb4c67f5f8f8b9b7
Karel Vervaeke <karel.vervaeke@conundra.eu>
2024-05-15 17:18:06 +0200

    tabs -> spaces

--- miditools/highlight.py
+++ miditools/highlight.py
@@ -4,41 +4,41 @@ from mido import MidiFile
 
 
 def highlight_track(in_path, out_path, track_name):
-	print(f"Converting {in_path} {track_name} -> {out_path}")
+  print(f"Converting {in_path} {track_name} -> {out_path}")
 
-	mid = MidiFile(in_path, clip=True)
+  mid = MidiFile(in_path, clip=True)
 
-	for i, track in enumerate(mid.tracks):
-		for msg in track:
+  for i, track in enumerate(mid.tracks):
+    for msg in track:
 
-			if track.name != track_name and msg.type == "note_on":
-				new_velocity = int(msg.velocity * .40)
-				msg.velocity = new_velocity
+      if track.name != track_name and msg.type == "note_on":
+        new_velocity = int(msg.velocity * .40)
+        msg.velocity = new_velocity
 
-	mid.save(out_path)
+  mid.save(out_path)
 
 
 def highlight_tracks(input_path):
-	mid = MidiFile(input_path, clip=True)
-	track_names = [track.name for track in mid.tracks]
+  mid = MidiFile(input_path, clip=True)
+  track_names = [track.name for track in mid.tracks]
 
-	for track_name in track_names:
-		dirname = os.path.dirname(input_path)
-		ext = ".mid"
-		if input_path.endswith(ext):
-			filename = os.path.basename(input_path)[:-len(ext)] + f" - {track_name}.hl.mid"
+  for track_name in track_names:
+    dirname = os.path.dirname(input_path)
+    ext = ".mid"
+    if input_path.endswith(ext):
+      filename = os.path.basename(input_path)[:-len(ext)] + f" - {track_name}.hl.mid"
 
-		output_path = os.path.join(dirname, filename)
-		highlight_track(input_path, output_path, track_name)
+    output_path = os.path.join(dirname, filename)
+    highlight_track(input_path, output_path, track_name)
 
 
 def main():
-	input_paths = sys.argv[1:]
+  input_paths = sys.argv[1:]
 
-	for input_path in input_paths:
-		if not input_path.endswith(".hl.mid"):
-			highlight_tracks(input_path)
+  for input_path in input_paths:
+    if not input_path.endswith(".hl.mid"):
+      highlight_tracks(input_path)
 
 
 if __name__ == "__main__":
-	main()
+  main()
